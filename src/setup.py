0cd922f71951f24add8a52418339ed3a926d3450
Dr. Jörg Richter <joerg.richter@nantoka.de>
2024-04-05 18:18:53 +0200

    Initial commit.

---
+++ src/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="python-simple-packaging-test",
+    version="1.0.0",
+    packages=["my_package"],
+    license="MIT",
+    author="Dr. Jörg Richter",
+    author_email="python-book@nantoka.de",
+    description="A simple packaging test"
+)
